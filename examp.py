ac11edc77b5e7e1dd116d101144728c64ea33b25
liilol <erick.mo0420@gmail.com>
2025-01-16 23:15:33 +0900

    test code

--- examp.py
+++ examp.py
@@ -1,2 +1,10 @@
-print("Hello World")
-print("test")
+def solution(num_list, n):
+    ans = [] 
+    while num_list: 
+        storge = []
+        for _ in range(n):
+            storge.append(num_list.pop(0))
+        ans.append(storge)
+    return ans
+
+solution([1, 2, 3, 4, 5, 6, 7, 8],2)
